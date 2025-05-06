883feba437bf169d90954336ba2511546449812b
codingdestro <codingdestro@gmail.com>
2025-05-06 22:42:39 +0530

    add: add book and delete book routes

--- book-store/controllers/books.go
+++ book-store/controllers/books.go
@@ -3,8 +3,12 @@ package controllers
 import (
 	"book-store/config"
 	"book-store/models"
+	"book-store/utils"
 	"encoding/json"
+	"fmt"
+	"io"
 	"net/http"
+
 	"github.com/gorilla/mux"
 )
 
@@ -15,11 +19,56 @@ func GetBooks(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(books)
 }
 
+func AddBook(w http.ResponseWriter, r *http.Request) {
+	db := config.GetDB()
+	data, err := io.ReadAll(r.Body)
+	if err != nil {
+		fmt.Fprintf(w, "no book got")
+		return
+	}
+	var book models.Book
+	err = json.Unmarshal(data, &book)
+	if err != nil {
+		fmt.Fprintln(w, "no book got")
+		return
+	}
+	book.Id = utils.GenerateID()
+	fmt.Println(book)
+	result := db.Create(book)
+	if result.Error != nil {
+		fmt.Fprint(w, "Failed to add new book!")
+		return
+	}
+	fmt.Fprint(w, "your book saved!")
+
+}
+
 func GetABook(w http.ResponseWriter, r *http.Request) {
 	db := config.GetDB()
 	params := mux.Vars(r)
 	bookId := params["id"]
 	var book models.Book
-	db.Where("id = ?", bookId).First(&book)
-	json.NewEncoder(w).Encode(book)
+	res := db.Where("id = ?", bookId).First(&book)
+	if res.Error != nil {
+		fmt.Fprintf(w, "failed to fetch you book id = %s", bookId)
+		return
+	}
+	jsonData, err := json.Marshal(book)
+	if err != nil {
+		fmt.Fprintf(w, "failed to fectch your book")
+		return
+	}
+	json.NewEncoder(w).Encode(jsonData)
+}
+
+func UpdateBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func DeleteBook(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	id := params["id"]
+	db := config.GetDB()
+	db.Where("id = ?", id).Delete(&models.Book{})
+	fmt.Fprintf(w, "Book has been deleted successfully")
 }
--- book-store/routes/router.go
+++ book-store/routes/router.go
@@ -9,6 +9,8 @@ import (
 func InitRoutes() *mux.Router {
 	router := mux.NewRouter()
 	router.HandleFunc("/", controllers.GetBooks).Methods("GET")
+	router.HandleFunc("/add", controllers.AddBook).Methods("POST")
 	router.HandleFunc("/{id}", controllers.GetABook).Methods("POST")
+	router.HandleFunc("/{id}", controllers.DeleteBook).Methods("DELETE")
 	return router
 }
